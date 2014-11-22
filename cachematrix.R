88b67a5c881bad10cafc69d29901ef2e39b56da7
lisken <nkolecka@gis.geo.uj.edu.pl>
2014-11-22 21:36:36 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## Includes 2 functions: 
+##    1.  makeCacheMatrix - creates a special "matrix" object 
+##        that can cache its inverse
+##    2.  cacheSolve - computes the inverse 
+##        of the special "matrix" returned by makeCacheMatrix 
+##        If the inverse has already been calculated 
+##        (and the matrix has not changed), then the cachesolve 
+##        should retrieve the inverse from the cache
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invm <- NULL
+  set <- function(y){
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setinvm <- function(inverse) invm <<- inverse
+  getinvm <- function() invm
+  list(set = set, get = get, 
+       setinvm = setinvm,
+       getinvm = getinvm)  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invm <- x$getinvm()
+  if(!is.null(invm)) {
+    message('getting cached data')
+    return(invm)
+  }
+  data <- x$get()
+  invm <- solve(data)
+  x$setinvm(invm)
+  invm
+  
 }
+
+# #Sample run:
+# x = matrix(rnorm(3*3), 3,3)
+# cm = makeCacheMatrix(x)
+# m$get()
+# 
+# 
+# # The first run
+# cacheSolve(cm)
+# 
+# 
+# # The second run - getting cache data
+# cacheSolve(m)
